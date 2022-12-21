07fc00609da55d67ab744aed9b8555b6ccc66054
SurovaDarina <121132419+SurovaDarina@users.noreply.github.com>
2022-12-21 10:20:45 +0300

    First Commit
    First Commit

---
+++ Планировка магазина1.2/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Планировка_магазина1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImage = global::Планировка_магазина1._2.Properties.Resources.kassa;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(14, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 151);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.BackgroundImage = global::Планировка_магазина1._2.Properties.Resources.plan;
+            this.panel1.Location = new System.Drawing.Point(371, 16);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(605, 707);
+            this.panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(161, 292);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 151);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImage = global::Планировка_магазина1._2.Properties.Resources.holod;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(161, 16);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 151);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(14, 292);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 151);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(14, 175);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 57);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Удалить";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(161, 175);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 57);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Удалить";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(14, 451);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(117, 57);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Удалить";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(161, 451);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(117, 57);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Удалить";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1015, 741);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Планировка магазина1.2/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Планировка_магазина1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); //инициалиация компонентов
+        }
+        PictureBox pct; //объявление PictureBox
+        public int sch = 0; //заводим счетчик для того, чтобы была возможность создавать несколько элементов
+        private void button1_Click(object sender, EventArgs e) //обработка кнопки кассы
+        {
+            pct = new PictureBox();
+            pct.Height = 119; // параметры элемента
+            pct.Width = 128;
+            pct.Name = "pct" + sch;
+            pct.Left = 20 + sch * 110; //координаты появления элемента
+            pct.Top = 30;
+            pct.Image = Properties.Resources.kassa; //прикрепляем картинку к PictureBox
+            panel1.Controls.Add(pct); //добавляем элемент на панель
+            sch += 1;
+            pct.MouseUp += pct_MouseUp; //ссылки на события
+            pct.MouseDown += pct_MouseDown;
+            pct.MouseMove += pct_MouseMove;
+        }
+        //реализация движения объектов
+        private bool Dragging1;
+        private int xPos1; //координаты элемента
+        private int yPos1;
+
+        private void pct_MouseUp(object sender, MouseEventArgs e) //если элемент не выбран
+        {
+            Dragging1 = false;
+        }
+
+        private void pct_MouseDown(object sender, MouseEventArgs e) //добавление элемента
+        {
+
+            if (e.Button == MouseButtons.Left) //если нажатa левая кнока мыши
+            {
+                Dragging1 = true;
+                xPos1 = e.X;
+                yPos1 = e.Y;
+            }
+        }
+
+        private void pct_MouseMove(object sender, MouseEventArgs e) // перенесение элемента по схеме
+        {
+            Control c = sender as Control;
+            if (Dragging1 && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos1;
+                c.Left = e.X + c.Left - xPos1;
+            }
+        }
+
+        PictureBox pictureBox1; //объявление PictureBox1
+        public int sch1 = 0;//заводим счетчик для того, чтобы была возможность создавать несколько элементов
+        private void button3_Click(object sender, EventArgs e) //обработка кнопки холодильника
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox1.Height = 200; //параметры элемента
+            pictureBox1.Width = 85;
+            pictureBox1.Name = "pictureBox1" + sch1;
+            pictureBox1.Left = 20 + sch1 * 110; //координаты появления элемента
+            pictureBox1.Top = 30;
+            pictureBox1.Image = Properties.Resources.holod;//прикрепляем картинку к PictureBox1
+            panel1.Controls.Add(pictureBox1); //добавляем элемент на панель
+            sch1 += 1;
+            pictureBox1.MouseUp += pictureBox1_MouseUp_1; //ссылки на события
+            pictureBox1.MouseDown += pictureBox1_MouseDown_1;
+            pictureBox1.MouseMove += pictureBox1_MouseMove_1;
+        }
+        //реализация движения элемента
+        private bool Dragging;
+        private int xPos;
+        private int yPos;
+
+        private void pictureBox1_MouseUp_1(object sender, MouseEventArgs e) //если элемент не выбран
+        {
+            Dragging = false;
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e) //добавление элемента
+        {
+            if (e.Button == MouseButtons.Left) //если мы нажимием на левую кнопку мыши
+            {
+                Dragging = true;
+                xPos = e.X;
+                yPos = e.Y;
+            }
+        }
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e) //перемещение элемента
+        {
+            Control c = sender as Control;
+            if (Dragging && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos;
+                c.Left = e.X + c.Left - xPos;
+            }
+        }
+
+        PictureBox pictureBox2; //объявление PictureBox2
+        public int sch2 = 0;//заводим счетчик для того, чтобы была возможность создавать несколько элементов
+        private void button4_Click(object sender, EventArgs e) //обработка кнопки 4
+        {
+            pictureBox2 = new PictureBox();
+            pictureBox2.Height = 120; //параметры элемента
+            pictureBox2.Width = 120;
+            pictureBox2.Name = "pictureBox2" + sch2;
+            pictureBox2.Left = 20 + sch2 * 110; //координаты появления элемента
+            pictureBox2.Top = 30;
+            pictureBox2.Image = Properties.Resources.showcase1; //прикрепляем картинку к PictureBox2
+            panel1.Controls.Add(pictureBox2);//добавляем элемент на панель
+            sch2 += 1;
+            pictureBox2.MouseUp += pictureBox2_MouseUp_2; //ссылки на события
+            pictureBox2.MouseDown += pictureBox2_MouseDown_2;
+            pictureBox2.MouseMove += pictureBox2_MouseMove_2;
+        }
+        //реализация движения элемента
+        private bool Dragging2;
+        private int xPos2;
+        private int yPos2;
+
+        private void pictureBox2_MouseUp_2(object sender, MouseEventArgs e)//если элемент не выбран
+        {
+            Dragging2 = false;
+        }
+        private void pictureBox2_MouseDown_2(object sender, MouseEventArgs e)//добавление элемента
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                Dragging2 = true;
+                xPos2 = e.X;
+                yPos2 = e.Y;
+            }
+        }
+
+        private void pictureBox2_MouseMove_2(object sender, MouseEventArgs e)//перемещение элемента
+        {
+            Control c = sender as Control;
+            if (Dragging2 && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos2;
+                c.Left = e.X + c.Left - xPos2;
+            }
+        }
+
+        PictureBox pictureBox3;//объявление PictureBox1
+        public int sch3 = 0;//заводим счетчик для того, чтобы была возможность создавать несколько элементов
+
+        private void button2_Click(object sender, EventArgs e)//обработка кнопки 2
+        {
+            pictureBox3 = new PictureBox();
+            pictureBox3.Height = 120;//параметры элемента
+            pictureBox3.Width = 120;
+            pictureBox3.Name = "pictureBox3" + sch3;
+            pictureBox3.Left = 20 + sch3 * 110; //координаты появления элемента
+            pictureBox3.Top = 30;
+            pictureBox3.Image = Properties.Resources.showcase3; //прикрепляем картинку к PictureBox
+            panel1.Controls.Add(pictureBox3); //добавление элемента на панель
+            sch3 += 1;
+            pictureBox3.MouseUp += pictureBox3_MouseUp_3; //ссылки на события
+            pictureBox3.MouseDown += pictureBox3_MouseDown_3;
+            pictureBox3.MouseMove += pictureBox3_MouseMove_3;
+        }
+        //реализация перемещения элемента
+        private bool Dragging3;
+        private int xPos3;
+        private int yPos3;
+
+        private void pictureBox3_MouseUp_3(object sender, MouseEventArgs e) //если элемент не выбран
+        {
+            Dragging3 = false;
+        }
+
+        private void pictureBox3_MouseDown_3(object sender, MouseEventArgs e) //добавление элемента
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                Dragging3 = true;
+                xPos3 = e.X;
+                yPos3 = e.Y;
+            }
+        }
+
+        private void pictureBox3_MouseMove_3(object sender, MouseEventArgs e)//перемещение элемента
+        {
+            Control c = sender as Control;
+            if (Dragging3 && c != null)
+            {
+                c.Top = e.Y + c.Top - yPos3;
+                c.Left = e.X + c.Left - xPos3;
+            }
+        }
+        //реализация кнопок удаления элементов
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pct.Dispose();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Dispose();
+            sch1 = 0;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Dispose();
+            sch2 = 0;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Dispose();
+            sch3 = 0;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
